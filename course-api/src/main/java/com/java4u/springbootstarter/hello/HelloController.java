f195fb6702a653ca248b7d6d49280476292c28df
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-27 20:50:53 +0530

    Adding First Controllers

---
+++ course-api/src/main/java/com/java4u/springbootstarter/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.java4u.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi , Welcome to First page";
+	}
+
+}
