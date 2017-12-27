6702647e137336a9e2936e1fe624eafe8b10e11d
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-28 03:01:23 +0530

    Committing Spring initlizr (start.srping.io)  base project

---
+++ springinitilizr-demo/src/main/java/com/java4u/springinitilizrdemo/springinitilizrdemo/SpringinitilizrDemoApplication.java
@@ -0,0 +1,12 @@
+package com.java4u.springinitilizrdemo.springinitilizrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinitilizrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinitilizrDemoApplication.class, args);
+	}
+}
